1e641979f0baad19d035f5b4e0df58bc5116da1a
Heaven <314664252@qq.com>
2016-11-01 16:41:11 +0800

    add downloadUtil

---
+++ NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.h
@@ -0,0 +1,13 @@
+//
+//  DownloadUtil.h
+//  NetworkUtils
+//
+//  Created by Heaven on 2016/11/1.
+//  Copyright © 2016年 Heaven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadUtil : NSObject
+
+@end
---
+++ NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.m
@@ -0,0 +1,13 @@
+//
+//  DownloadUtil.m
+//  NetworkUtils
+//
+//  Created by Heaven on 2016/11/1.
+//  Copyright © 2016年 Heaven. All rights reserved.
+//
+
+#import "DownloadUtil.h"
+
+@implementation DownloadUtil
+
+@end
