cdf7299af8365f11bcb4e7c97db4d596edbe2a5e
Heaven <314664252@qq.com>
2016-11-02 11:15:39 +0800

    add downloadUtil

--- NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.h
+++ NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.h
@@ -8,6 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
+#define Folder [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask, YES) objectAtIndex:0]
+#define PathNAME @"Download"
+#define DownloadPath() [Folder stringByAppendingPathComponent:PathNAME]
+
+typedef void(^downloadSuccessBlock)(NSURLResponse *response, NSURL *filePath);
+typedef void(^downloadFailureBlock)(NSURL *, NSError *error);
+typedef void(^downloadProgressBlock)(NSProgress *progress);
+
 @interface DownloadUtil : NSObject
 
+/**
+ 开启下载任务
+ 
+ @param downloadUrl downloadUrl
+ @param filePath filePath
+ @param success success
+ @param failure failure
+ @param progress progress
+ */
+- (void)downloadWithUrl:(NSString *)downloadUrl filePath:(NSString *)filePath downloadSuccess:(downloadSuccessBlock)success downloadFailure:(downloadFailureBlock)failure downloadProgress:(downloadProgressBlock)progress;
+
+/** 单例方法 */
++ (instancetype)shareUtil;
+/** 取消单个下载 */
+- (void)cancelDownloadWithUrl:(NSString *)downloadUrl;
+/** 全部取消 */
+- (void)cancelAllDownload;
+/** 暂停单个下载 */
+- (void)pauseDownloadWithUrl:(NSString *)downloadUrl;
+/** 暂停全部下载 */
+- (void)pauseAllDownload;
+/** 重新开启单个下载 */
+- (void)resumeDownloadWithUrl:(NSString *)downloadUrl;
+/** 重新开启全部下载 */
+- (void)resumeAllDownload;
 @end
--- NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.m
+++ NetworkUtils/NetworkUtils/DownloadUtil/DownloadUtil.m
@@ -7,7 +7,198 @@
 //
 
 #import "DownloadUtil.h"
+#import "AFNetworking.h"
+
+@interface DownloadUtil ()
+/** 下载URL数组 */
+@property (nonatomic, strong) NSMutableArray *downloadUrlArray;
+/** 下载任务数组 */
+@property (nonatomic, strong) NSMutableArray *downloadTaskArray;
+@end
 
 @implementation DownloadUtil
+
++ (instancetype)shareUtil {
+
+    static id instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        instance = [[DownloadUtil alloc] init];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+
+    self = [super init];
+    if (self) {
+        
+        // 创建下载文件夹
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        [fileManager createDirectoryAtPath:DownloadPath() withIntermediateDirectories:YES attributes:nil error:nil];
+
+        _downloadUrlArray = [[NSMutableArray alloc] init];
+        _downloadTaskArray = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)downloadWithUrl:(NSString *)downloadUrl filePath:(NSString *)filePath downloadSuccess:(downloadSuccessBlock)success downloadFailure:(downloadFailureBlock)failure downloadProgress:(downloadProgressBlock)progress {
+
+    // 1. 查重，防止开启多个相同文件的下载任务
+    if ([_downloadUrlArray containsObject:downloadUrl]) {
+        NSLog(@"此任务已经存在任务列表");
+        return;
+    }
+    
+    // 2. URL处理，添加到下载URL数组
+    NSURL *URL = [NSURL URLWithString:downloadUrl];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    [_downloadUrlArray addObject:downloadUrl];
+    
+    // 3. 开启下载任务，添加到下载任务数组
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+        
+        return [NSURL URLWithString:filePath];
+        
+    } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+        
+        if (error) {
+            
+            // 错误的回调
+            failure(filePath, error);
+            [self removeDownloadedDataWithUrl:downloadUrl];
+        } else {
+            
+            // 成功的回调
+            success(response, filePath);
+            [self removeDownloadedDataWithUrl:downloadUrl];
+        }
+        
+    }];
+
+    [downloadTask resume];
+    
+    [_downloadTaskArray addObject:downloadTask];
+    
+}
+
+/** 内部方法，清空已下载数据的内存 */
+- (void)removeDownloadedDataWithUrl:(NSString *)downloadUrl {
+
+    if (downloadUrl == nil || _downloadUrlArray.count || _downloadTaskArray.count == 0) {
+        return;
+    }
+    
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        
+        NSString *tagetDownloadUrl = [_downloadUrlArray objectAtIndex:i];
+        if ([tagetDownloadUrl isEqualToString:downloadUrl]) {
+            
+            [_downloadUrlArray removeObjectAtIndex:i];
+            [_downloadUrlArray removeObjectAtIndex:i];
+            
+            break;
+        }
+    }
+
+}
+
+- (void)cancelDownloadWithUrl:(NSString *)downloadUrl {
+    
+    // 容错，防止downloadUrl为空
+    if (downloadUrl == nil || _downloadUrlArray.count || _downloadTaskArray.count == 0) {
+        return;
+    }
+}
+
+- (void)cancelAllDownload {
+
+    if (_downloadUrlArray.count == 0 || _downloadTaskArray.count == 0) {
+        return;
+    }
+    
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        NSURLSessionDownloadTask *downloadTask = [_downloadTaskArray objectAtIndex:i];
+        
+        if (downloadTask.state == NSURLSessionTaskStateRunning || downloadTask.state == NSURLSessionTaskStateSuspended) {
+            [downloadTask cancel];
+        }
+    }
+
+}
+
+- (void)pauseDownloadWithUrl:(NSString *)downloadUrl {
+    // 容错，防止downloadUrl为空
+    if (downloadUrl == nil || _downloadUrlArray.count || _downloadTaskArray.count == 0) {
+        return;
+    }
+    
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        
+        NSString *tagetDownloadUrl = [_downloadUrlArray objectAtIndex:i];
+        if ([tagetDownloadUrl isEqualToString:downloadUrl]) {
+            NSURLSessionDownloadTask *downloadTask = [_downloadTaskArray objectAtIndex:i];
+            if (downloadTask.state == NSURLSessionTaskStateRunning) {
+                [downloadTask suspend];
+            }
+            break;
+        }
+    }
+}
+
+- (void)pauseAllDownload {
+
+    if (_downloadUrlArray.count == 0 || _downloadTaskArray.count == 0) {
+        return;
+    }
+    
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        NSURLSessionDownloadTask *downloadTask = [_downloadTaskArray objectAtIndex:i];
+        
+        if (downloadTask.state == NSURLSessionTaskStateRunning) {
+            [downloadTask suspend];
+        }
+    }
+}
+
+- (void)resumeDownloadWithUrl:(NSString *)downloadUrl {
+    // 容错，防止downloadUrl为空
+    if (downloadUrl == nil || _downloadUrlArray.count || _downloadTaskArray.count == 0) {
+        return;
+    }
+
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        
+        NSString *tagetDownloadUrl = [_downloadUrlArray objectAtIndex:i];
+        if ([tagetDownloadUrl isEqualToString:downloadUrl]) {
+            NSURLSessionDownloadTask *downloadTask = [_downloadTaskArray objectAtIndex:i];
+            if (downloadTask.state == NSURLSessionTaskStateSuspended) {
+                [downloadTask resume];
+            }
+            break;
+        }
+    }
+}
+
+- (void)resumeAllDownload {
+
+    if (_downloadUrlArray.count == 0) {
+        return;
+    }
+    
+    for (int i = 0; _downloadUrlArray.count; ++i) {
+        NSURLSessionDownloadTask *downloadTask = [_downloadTaskArray objectAtIndex:i];
+        if (downloadTask.state == NSURLSessionTaskStateSuspended) {
+            [downloadTask resume];
+        }
+    }
+}
 
 @end
